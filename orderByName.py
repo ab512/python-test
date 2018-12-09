202592072e9e7b245322ec2d72501366bb32d4a0
cy <121836816@qq.com>
2018-12-09 22:43:06 +0800

    按姓名排序

---
+++ orderByName.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+def _by_name(x):
+    return x[0].lower()
+
+
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+
+orderTuple = sorted(L, key=_by_name)
+print(orderTuple)
